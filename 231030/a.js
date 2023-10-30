707d7679b08b8b023b0f5d7dfea0ef816882e97d
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2023-10-30 16:31:49 +0900

    231030

---
+++ 231030/a.js
@@ -0,0 +1,13 @@
+const myProfile = {
+  name: "h662",
+  age: 20,
+  isRich: false,
+};
+
+console.log(myProfile);
+
+//Object.freeze(myProfile);
+
+myProfile.computer = "윈도우";
+
+console.log(myProfile);
