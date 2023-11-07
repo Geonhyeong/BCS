506230b84312c55397f9f7881d122797969cd462
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2023-11-07 09:24:28 +0900

    231106

---
+++ 231106/tw/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="bg-pink-100 min-h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-20 py-40">
+      <div className="bg-red-500 card-style">Card</div>
+      <div className="bg-orange-500 card-style">Card</div>
+      <div className="bg-yellow-500 card-style">Card</div>
+      <div className="bg-green-500 card-style">Card</div>
+      <div className="bg-blue-500 card-style">Card</div>
+      <div className="bg-indigo-500 card-style">Card</div>
+      <div className="bg-purple-500 card-style">Card</div>
+    </div>
+  );
+};
+
+export default App;
