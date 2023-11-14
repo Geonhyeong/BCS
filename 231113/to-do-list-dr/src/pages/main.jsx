bfad4114508cc2664d28e90846f2b04406c28095
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2023-11-14 17:29:44 +0900

    231114

--- 231113/to-do-list-dr/src/pages/main.jsx
+++ 231113/to-do-list-dr/src/pages/main.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import CreateTodo from "../components/CreateTodo";
 import { useEffect } from "react";
+import TodoCard from "../components/TodoCard";
 
 const Main = () => {
   const [todos, setTodos] = useState([]);
@@ -22,8 +23,6 @@ const Main = () => {
     getTodos();
   }, []);
 
-  useEffect(() => console.log(lastTodoId), [lastTodoId]);
-
   return (
     <main className="min-h-screen max-w-screen-md mx-auto">
       <h1 className="text-center text-4xl font-bold py-12">To do list</h1>
@@ -32,15 +31,7 @@ const Main = () => {
         {todos.length === 0
           ? "비어있을 때"
           : todos.map((v, i) => {
-              return (
-                <li className="h-12 flex items-center text-xl">
-                  <span className="w-1/12 text-right font-semibold">
-                    {v.id}
-                  </span>
-                  <span className="w-8/12 pl-2 ml-4">{v.title}</span>
-                  <button className="w-3/12 hover:font-bold">Detail</button>
-                </li>
-              );
+              return <TodoCard key={i} todo={v} />;
             })}
       </ul>
     </main>
