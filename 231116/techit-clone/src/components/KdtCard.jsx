402ce04cff90270dfffdcdea526624b0493da1f3
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2023-11-20 16:30:01 +0900

    Schedule Section

--- 231116/techit-clone/src/components/KdtCard.jsx
+++ 231116/techit-clone/src/components/KdtCard.jsx
@@ -1,20 +1,20 @@
-const KdtCard = () => {
+const KdtCard = ({ image, title, ing, desc, period }) => {
   return (
-    <div className="bg-yellow-100 w-[290px] h-[416px]">
+    <div className="w-[290px] h-[416px]">
       <div className="overflow-hidden rounded-lg">
         <img
           className="hover:scale-110 duration-300 rounded-lg"
-          src="./images/kdt_blockchain.webp"
+          src={`./images/${image}.webp`}
           alt="kdt_blockchain"
         />
       </div>
-      <div>
+      <div className="pt-4">
         <span className="text-[#1d4ed8] text-sm font-semibold border border-[#1d4ed8] py-1 px-2 rounded-[4px]">
           모집중
         </span>
-        <h3>블록체인 스쿨</h3>
-        <h5>미래를 바꿀 대체불가한 개발자의 시작</h5>
-        <div>23.11.01(수) ~ 23.11.30(목) 까지</div>
+        <h3 className="pt-4 py-2 text-xl font-semibold">{title}</h3>
+        <h5 className="text-lg">{desc}</h5>
+        <div className="text-techit-gray-100 mt-4">{period}</div>
       </div>
     </div>
   );
